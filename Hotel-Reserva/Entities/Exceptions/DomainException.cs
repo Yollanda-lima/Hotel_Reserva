3c9babb90143412df9d66f93cb042841bab65d37
Yollanda Lima <yollandalima123@gmail.com>
2023-06-28 12:28:45 -0300

    Projeto reserva de hotel criado

---
+++ Hotel-Reserva/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hotel_Reserva.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
